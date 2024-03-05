2fff4ea61485adafb2442b028f72a4d8ff96f972
endloc <li.amirova.l@gmail.com>
2024-03-06 02:49:19 +0300

    tests: successful Login and Register tests

--- handler/auth_test.go
+++ handler/auth_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	mock2 "github.com/stretchr/testify/mock"
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
 	"net/http/httptest"
@@ -15,88 +16,132 @@ import (
 	"testing"
 )
 
-func TestLogin(t *testing.T) {
-	mock := mocks.NewMethods(t)
-	handler := APIHandler{Connector: mock}
-	type mockBehavior func()
+type test struct {
+	name               string
+	reqBody            map[string]string
+	mockBehavior       mockBehavior
+	expectedStatusCode int
+	expectedRespBody   string
+}
 
-	users := []db.User{
-		{
-			UserID:   123,
+type mockBehavior func()
+
+// mock := mocks.NewMethods(t)
+var (
+	mock         = &mocks.Methods{}
+	handler      = APIHandler{Connector: mock}
+	loginPath    = "/api/v1/login"
+	registerPath = "/api/v1/register"
+	logoutPath   = "/api/v1/logout"
+	users        = map[string]db.User{
+		"200 OK Simple": {
+			UserID:   1,
 			Email:    "example@gmail.com",
 			Nickname: "example",
 			Password: "ExampleE1",
 		},
-		{
+		"200 OK Cyrillic alphabet in email": {
 			UserID:   2,
-			Email:    "TesT.here@что-то.рф",
-			Nickname: "test1",
-			Password: "Testtest1T",
+			Email:    "москва.тут@москва.рф",
+			Nickname: "Moscow",
+			Password: "MoscowTest1",
+		},
+		"200 OK Capital letters in email": {
+			UserID:   3,
+			Email:    "TesT.her_e@sth.ru",
+			Nickname: "Michael",
+			Password: "1918Michael",
+		},
+		"200 OK Crazy (valid) email": {
+			UserID:   4,
+			Email:    "Crazy%e.{m}ail~@$trange.com",
+			Nickname: "crazy_user",
+			Password: "crazyUser24",
+		},
+		"200 OK Max nickname and password length": {
+			UserID:   1982897323,
+			Email:    "a@b.c",
+			Nickname: "crazy_user_2crazy_us",
+			Password: "crazyUser24ksHokssn27awb",
 		},
 	}
+)
 
-	var hashPasswords []string
-	for _, user := range users {
-		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+func PostTypeTestsExecution(t *testing.T, path string, tests []test) {
+	for _, curTest := range tests {
+		t.Run(curTest.name, func(t *testing.T) {
+			reqBytes, err := json.Marshal(curTest.reqBody)
+			if err != nil {
+				t.Errorf("error marshaling request body: %v", err)
+			}
+			curTest.mockBehavior()
+			recorder := httptest.NewRecorder()
+			req, err := http.NewRequest("POST", path, bytes.NewBuffer(reqBytes))
+			if err != nil {
+				t.Errorf("error creating request: %v", err)
+			}
+			switch path {
+			case loginPath:
+				handler.Login(recorder, req)
+			case registerPath:
+				handler.Register(recorder, req)
+			}
+			assert.Equal(t, recorder.Code, curTest.expectedStatusCode)
+			assert.Equal(t, recorder.Body.String(), curTest.expectedRespBody)
+		})
+	}
+}
+
+func TestLoginSuccess(t *testing.T) {
+	var tests []test
+
+	for testName, user := range users {
+		curHashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 		if err != nil {
 			t.Errorf("error hashng passwords: %v", err)
 			return
 		}
-		hashPasswords = append(hashPasswords, string(hashedPassword))
-	}
-
-	tests := []struct {
-		name               string
-		reqBody            map[string]string
-		mockBehavior       mockBehavior
-		expectedStatusCode int
-		expectedRespBody   string
-	}{
-		{
-			name: "OK",
+		curTest := test{
+			name: testName,
 			reqBody: map[string]string{
-				"email":    users[0].Email,
-				"password": users[0].Password,
+				"email":    user.Email,
+				"password": user.Password,
 			},
 			mockBehavior: func() {
-				users[0].Password = hashPasswords[0]
-				mock.On("GetUserByEmail", users[0].Email).Return(users[0], nil).Once()
+				user.Password = string(curHashedPassword)
+				mock.On("GetUserByEmail", user.Email).Return(user, nil).Once()
 			},
 			expectedStatusCode: 200,
 			expectedRespBody: fmt.Sprintf(`{"user_id":%d,"email":"%s","nickname":"%s"}`,
-				users[0].UserID, users[0].Email, users[0].Nickname),
-		},
-		{
-			name: "OK",
+				user.UserID, user.Email, user.Nickname),
+		}
+		tests = append(tests, curTest)
+	}
+
+	PostTypeTestsExecution(t, loginPath, tests)
+}
+
+func TestRegisterSuccess(t *testing.T) {
+	var tests []test
+
+	for testName, user := range users {
+		curTest := test{
+			name: testName,
 			reqBody: map[string]string{
-				"email":    users[1].Email,
-				"password": users[1].Password,
+				"email":    user.Email,
+				"nickname": user.Nickname,
+				"password": user.Password,
 			},
 			mockBehavior: func() {
-				users[1].Password = hashPasswords[1]
-				mock.On("GetUserByEmail", users[1].Email).Return(users[1], nil).Once()
+				mock.On("RegisterUser", mock2.AnythingOfType("db.User")).Return(nil)
+				mock.On("GetUserByEmail", user.Email).Return(user, nil)
 			},
 			expectedStatusCode: 200,
 			expectedRespBody: fmt.Sprintf(`{"user_id":%d,"email":"%s","nickname":"%s"}`,
-				users[1].UserID, users[1].Email, users[1].Nickname),
-		},
+				user.UserID, user.Email, user.Nickname),
+		}
+		tests = append(tests, curTest)
 	}
 
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			reqBytes, err := json.Marshal(test.reqBody)
-			if err != nil {
-				t.Errorf("error marshaling request body: %v", err)
-			}
-			test.mockBehavior()
-			recorder := httptest.NewRecorder()
-			req, err := http.NewRequest("POST", "/api/v1/login", bytes.NewBuffer(reqBytes))
-			if err != nil {
-				t.Errorf("error creating request: %v", err)
-			}
-			handler.Login(recorder, req)
-			assert.Equal(t, recorder.Code, test.expectedStatusCode)
-			assert.Equal(t, recorder.Body.String(), test.expectedRespBody)
-		})
-	}
+	PostTypeTestsExecution(t, registerPath, tests)
 }
