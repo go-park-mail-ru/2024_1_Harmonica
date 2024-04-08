9545b95703297bad75b206ba150f4b93e9057b58
Gavriil <gavriil.belov@yandex.ru>
2024-04-08 21:14:01 +0300

    HAR-34: Add samesite 'none'.

--- internal/handler/middleware/csrf.go
+++ internal/handler/middleware/csrf.go
@@ -8,7 +8,6 @@ import (
 
 func CSRF(next http.Handler) http.Handler {
 	return csrf.Protect([]byte("32-byte-long-auth-key"),
+		csrf.SameSite(csrf.SameSiteNoneMode),
 		csrf.HttpOnly(true))(next)
 }
-
-//csrf.SameSite(csrf.SameSiteNoneMode)
