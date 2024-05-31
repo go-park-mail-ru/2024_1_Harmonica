7ce841e7d976ccab7036bd2e97027700db84f72d
glebkostin <glebkos777@gmail.com>
2024-05-31 19:16:39 +0300

    HAR-148: Скрипты для Get

---
+++ internal/repository/perf_test/get_wrk_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Отправляем GET-запрос на ручку is_auth и получаем CSRF-токен и _gorilla_csrf из заголовков ответа
+response_headers=$(curl -s -D - -X GET \
+     -H "Content-Type: application/json" \
+     -H "Origin: https://harmoniums.ru" \
+     https://harmoniums.ru/api/v1/is_auth)
+
+csrf_token=$(echo "$response_headers" | grep -i 'X-Csrf-Token:' | sed 's/^X-Csrf-Token: //i' | tr -d '\r')
+gorilla_csrf=$(echo "$response_headers" | grep -i 'Set-Cookie: _gorilla_csrf' | sed 's/.*_gorilla_csrf=\([^;]*\).*/\1/' | tr -d '\r')
+
+wrk -t6 -c10 -d10m https://harmoniums.ru \
+    -H "X-CSRF-Token: ${csrf_token}" \
+    -H "Cookie: _gorilla_csrf=${gorilla_csrf};" \
+    -s ./get_wrk_test.lua
